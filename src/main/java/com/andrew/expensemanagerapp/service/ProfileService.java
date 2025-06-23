12897ae3d5b7738d015ea1ffbe6321a800d54bbd
AndrewUket <andiuket@gmail.com>
2025-06-23 21:14:18 +0100

    Create my securityFilterChain to allow from certain address and disable default security configurations

--- src/main/java/com/andrew/expensemanagerapp/service/ProfileService.java
+++
@@ -1,8 +0,0 @@
-package com.andrew.expensemanagerapp.service;
-
-import com.andrew.expensemanagerapp.request.ProfileRequest;
-import com.andrew.expensemanagerapp.request.ProfileResponse;
-
-public interface ProfileService {
-    ProfileResponse createProfile(ProfileRequest request);
-}
